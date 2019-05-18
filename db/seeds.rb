ce3b8398a6ae5272ce46f38e46d88df9089c3e63
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-05-18 22:38:28 +0200

    Add logo design and layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.delete_all
-Product.create!(title: 'Samsung Chromebook (Wi-Fi, 11.6-Inch) - Silver (Renewed)',
+Product.create!(title: 'Acer R11  2-in-1 Chromebook, 11.6in HD Touchscreen, Chrome OS ',
   description:
     %{<p>
-      <em>The Samsung Chromebook</em> is a new computer that helps you get everyday things done faster and easier. It starts in seconds, has virus protection built-in, and runs your favorite Google apps plus thousands more. The Chromebook comes with leading Google products, like Search, Gmail, YouTube and Hangouts, so you can work, play, and do whatever you want, right out of the box. Chromebooks are a unique class of mobile computing devices designed for Web-based tasks. They run the Google Chrome OS, are perfect for browsing the Web, sending and receiving e-mail, and video chatting with friends and family. You can easily share it with multiple people - everyone gets their own files, apps and settings. And it's simple to use.
+      Acer R11 convertible 2-in-1 Chromebook: carry your work as you travel with this Acer Chromebook laptop computer. It's powered by an Intel Celeron processor with 4 GB of ram so programs run smoothly, and it has 32 GB of flash storage to hold plenty of files. This Acer Chromebook laptop computer has an 11.6-inch screen for clear, sharp graphics images. Product overview: 11.6" 10-point multitouch screen for hands-on control. 360 degree Flip-and-fold design: provides versatile functionality with laptop, audience, tabletop, presentation and tablet modes. It weighs 2.8lb. and is 0.8" thin: ultraportable design, featuring a smaller screen size to achieve a compact form factor. 3-cell lithium-ion battery.
       </p>},
-  image_url: 'samsung11.6.jpg',    
-  price: 119.00)
+  image_url: 'acer.jpg',    
+  price: 217.00)
 # . . .
-Product.create!(title: 'Corsair Gaming K65 LUX RGB Compact Mechanical Keyboard (Renewed)',
+Product.create!(title: 'Corsair Gaming K65 LUX RGB Compact Mechanical Keyboard',
   description:
     %{<p>
       <em>With virtually unlimited customization directly integrated into Corsair's legendary construction</em>, the LUX line of gaming keyboards is the key to breaking through the competition. Express yourself with programmable advanced lighting control and large font keycaps, and transform your gameplay with on-the-fly macro programming. Loaded into a rugged aluminum body, Corsair LUX keyboards provide the ultimate experience for every situation.
@@ -23,7 +23,7 @@ Product.create!(title: 'Corsair Gaming K65 LUX RGB Compact Mechanical Keyboard (
   price: 128.00)
 # . . .
 
-Product.create!(title: 'Apple iPhone 6S AT&T 16GB - Gold (Renewed)',
+Product.create!(title: 'Apple iPhone 6S AT&T 16GB - Gold',
   description:
     %{<p>
       <em>This update to the popular iPhone 6 adds 3D Touch Control</em> which lets users deliberately choose between a light tap, a press, and a "deeper" press,triggering a range of specific controls. Other notable additions include the Apple A9 chipset, and a 12MP rear camera with 4K resolution video recording. This Amazon Certified device is in EXCELLENT cosmetic condition, it is 100% fully functional as tested by our expert technicians and passed an industry leading 65-point inspection. The device may have minor cosmetic wear such as minimal light scratches on the screen and minor imperfections on sides and/or back. The device has been thoroughly inspected, cleaned and refurbished (if necessary) at our manufacturer approved repair center.
