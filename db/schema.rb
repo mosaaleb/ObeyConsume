60bb9b8647d787048d669fcff0300975b66138f6
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-05-19 01:39:22 +0200

    cart scaffold && set_card method

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-;# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_164213) do
+ActiveRecord::Schema.define(version: 2019_05_18_223100) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
