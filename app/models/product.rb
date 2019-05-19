10212674a59d5884c60aa448c85aef72d872aa50
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-05-19 02:55:51 +0200

    created line-item with its associations to cart and product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  # association
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  # validation
   validates :title, :description, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 1 }
   validates :title, uniqueness: true
@@ -6,4 +12,14 @@ class Product < ApplicationRecord
             with: %r{\.(gif|jpg|png)\Z}i, 
             message: 'must be a url for GIF, JPG or PNG image'
           }
+
+  private
+
+  # ensure that there is no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
