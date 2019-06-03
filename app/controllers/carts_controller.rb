215ce75a4aba44fc2ca265f3aa7eb4026c0fe5f7
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-06-03 10:23:12 +0200

    create line item model

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,61 @@
+class CartsController < ApplicationController
+  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @carts = Cart.all
+  end
+
+  def show
+  end
+
+  def new
+    @cart = Cart.new
+  end
+
+  def edit
+  end
+
+  def create
+    @cart = Cart.new(cart_params)
+
+    respond_to do |format|
+      if @cart.save
+        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
+        format.json { render :show, status: :created, location: @cart }
+      else
+        format.html { render :new }
+        format.json { render json: @cart.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @cart.update(cart_params)
+        format.html { redirect_to @cart, notice: 'Cart was successfully updated.' }
+        format.json { render :show, status: :ok, location: @cart }
+      else
+        format.html { render :edit }
+        format.json { render json: @cart.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @cart.destroy
+    respond_to do |format|
+      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  
+  def set_cart
+    @cart = Cart.find(params[:id])
+  end
+
+  def cart_params
+    params.fetch(:cart, {})
+  end
+end
---
+++ app/controllers/line_items_controller.rb
@@ -0,0 +1,63 @@
+class LineItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:create]
+  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @line_items = LineItem.all
+  end
+
+  def show
+  end
+
+  def new
+    @line_item = LineItem.new
+  end
+
+  def edit
+  end
+
+  def create
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(product: product)
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.json { render :show, status: :created, location: @line_item }
+      else
+        format.html { render :new }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @line_item.update(line_item_params)
+        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @line_item }
+      else
+        format.html { render :edit }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @line_item.destroy
+    respond_to do |format|
+      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_line_item
+      @line_item = LineItem.find(params[:id])
+    end
+
+    def line_item_params
+      params.require(:line_item).permit(:product_id, :cart_id)
+    end
+end
